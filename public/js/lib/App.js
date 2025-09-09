f32ccdc3dafcc4bd8b5d5c2bcf1ff5eac8d0588b
beefoo <brian@youaremyjoy.org>
2025-09-09 11:08:28 -0400

    Implement drag glass

--- public/js/lib/App.js
+++ public/js/lib/App.js
@@ -1,3 +1,4 @@
+import MathHelper from './MathHelper.js';
 import PointerManager from './PointerManager.js';
 
 export default class App {
@@ -10,6 +11,48 @@ export default class App {
   }
 
   init() {
-    this.pointers = new PointerManager({});
+    this.$table = document.getElementById('table');
+    this.offset = {
+      x: 0,
+      y: 0,
+    };
+    this.pointers = new PointerManager({
+      onDrag: (pointer) => {
+        this.onDragGlass(pointer);
+      },
+      target: 'glass',
+    });
+    this.onResize();
+  }
+
+  loadListeners() {
+    window.addEventListener('resize', (_event) => this.onResize());
+  }
+
+  onDragGlass(pointer) {
+    if (!pointer.isPrimary) return;
+    const { w, h } = this.tableSize;
+    const [xmin, ymin, xmax, ymax] = [-25, -25, 25, 25];
+    const { delta } = pointer;
+    this.offset.x = MathHelper.clamp(
+      this.offset.x + (delta.x / w) * 100,
+      xmin,
+      xmax,
+    );
+    this.offset.y = MathHelper.clamp(
+      this.offset.y + (delta.y / h) * 100,
+      ymin,
+      ymax,
+    );
+    const { x, y } = this.offset;
+    this.$table.style.transform = `translate3d(${x}%, ${y}%, 0)`;
+  }
+
+  onResize() {
+    const rect = this.$table.getBoundingClientRect();
+    this.tableSize = {
+      w: rect.width,
+      h: rect.height,
+    };
   }
 }
