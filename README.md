5b88096998cb800bd35b2b9fa6c6885fe2f94672
beefoo <brian@youaremyjoy.org>
2025-09-28 09:18:29 -0400

    Implement extract notebook to extract svg characters from an image

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # The Decoded DJ
+
+## Running scripts
+
+Install [Tesseract](https://github.com/tesseract-ocr/tesseract) to do the OCR. Also download [eng.traineddata](https://github.com/tesseract-ocr/tessdata/blob/main/eng.traineddata) and place in `path/to/Tesseract-OCR/tessdata/` to support running legacy Tesseract engine.
+
+Then install Python libraries:
+
+```
+pip install -r requirements.txt
+```
--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,8 +1,14 @@
 """Helper functions"""
 
 import glob
+import io
 import json
 import os
+
+from PIL import Image
+from potrace import Bitmap
+import pytesseract
+import requests
 
 
 def clear_directory(dirname):
@@ -15,6 +21,122 @@ def clear_directory(dirname):
             os.remove(fn)
 
 
+def add_confidences(chars, words):
+    """
+    Given characters with bounding boxes, match it with their words' confidences
+    """
+    for i, char in enumerate(chars):
+        confidence = -1
+        # First check if the char is contained in a word
+        word_matches = [
+            word
+            for word in words
+            if rect_contains(word[2:], char[1:]) and char[0] in word[0]
+        ]
+        if len(word_matches) > 0:
+            confidence = word_matches[0][1]
+        # Otherwise, check for the largest overlap
+        else:
+            word_matches = [
+                (word, rect_intersection_area(word[2:], char[1:]))
+                for word in words
+                if rect_intersection_area(word[2:], char[1:]) > 0 and char[0] in word[0]
+            ]
+            if len(word_matches) > 0:
+                word_matches = sorted(word_matches, key=lambda m: -m[1])
+                confidence = word_matches[0][0][1]
+
+        chars[i] = char + tuple([confidence])
+
+    return chars
+
+
+def extract_letters(im, letters, tesseract_cmd, conf=90):
+    """
+    Given an image and a list of letters, extract those letters from the image
+    """
+
+    pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
+
+    w, h = im.size
+    ocr_data = pytesseract.image_to_data(im, output_type="data.frame")
+    ocr_data = ocr_data[ocr_data["text"].notnull()]
+    ocr_data = ocr_data[ocr_data["conf"] > 0]
+    ocr_data = ocr_data.to_dict("records")
+    words = [
+        (
+            d["text"],
+            d["conf"],
+            d["left"],
+            d["top"],
+            d["left"] + d["width"],
+            d["top"] + d["height"],
+        )
+        for d in ocr_data
+    ]
+    # we need to use the legacy engine to get bounding boxes bc the newer one does not do bounding boxes well
+    ocr_chars = pytesseract.image_to_boxes(im, config="--oem 0")
+    char_data = [tuple(line.split(" ")) for line in ocr_chars.splitlines()]
+    chars = [
+        (char, int(x0), h - int(y1), int(x1), h - int(y0))
+        for char, x0, y0, x1, y1, _ in char_data
+        if char in letters
+    ]
+    chars = add_confidences(chars, words)
+    chars = [c for c in chars if c[-1] >= conf]
+
+    return (words, chars)
+
+
+def get_image_from_url(url):
+    """
+    Download and open an image from a URL to
+    """
+    # Download the image to memory
+    response = requests.get(url, timeout=60)
+    image_filestream = io.BytesIO(response.content)
+
+    # And read the image data
+    im = Image.open(image_filestream)
+
+    return im
+
+
+def image_to_svg(im):
+    w, h = im.size
+    bm = Bitmap(im, blacklevel=0.5)
+    plist = bm.trace(
+        turdsize=2,
+        turnpolicy=4,
+        alphamax=1,
+        opticurve=True,
+        opttolerance=0.2,
+    )
+
+    parts = []
+    for curve in plist:
+        fs = curve.start_point
+        parts.append(f"M{fs.x},{fs.y}")
+        for segment in curve.segments:
+            if segment.is_corner:
+                a = segment.c
+                b = segment.end_point
+                parts.append(f"L{a.x},{a.y}L{b.x},{b.y}")
+            else:
+                a = segment.c1
+                b = segment.c2
+                c = segment.end_point
+                parts.append(f"C{a.x},{a.y} {b.x},{b.y} {c.x},{c.y}")
+        parts.append("z")
+    svg = f'<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="{w}" height="{h}" viewBox="0 0 {w} {h}">'
+    svg += (
+        f'<path stroke="none" fill="black" fill-rule="evenodd" d="{"".join(parts)}"/>'
+    )
+    svg += "</svg>"
+
+    return svg
+
+
 def make_directories(filenames):
     """Function for creating directories if they do not exist."""
     if not isinstance(filenames, list):
@@ -23,6 +145,29 @@ def make_directories(filenames):
         dirname = os.path.dirname(filename)
         if not os.path.exists(dirname):
             os.makedirs(dirname)
+
+
+def rect_contains(rect_outer, rect_inner):
+    """
+    Given two rectangles, return if the first contains the second
+    """
+    o_x1, o_y1, o_x2, o_y2 = rect_outer
+    i_x1, i_y1, i_x2, i_y2 = rect_inner
+    return o_x1 <= i_x1 < i_x2 <= o_x2 and o_y1 <= i_y1 < i_y2 <= o_y2
+
+
+def rect_intersection_area(rect_a, rect_b):
+    """
+    Given two rectangles, return their area of intersection
+    Return 0 if they do not intersect
+    """
+    a_x1, a_y1, a_x2, a_y2 = rect_a
+    b_x1, b_y1, b_x2, b_y2 = rect_b
+    dx = min(a_x2, b_x2) - max(a_x1, b_x1)
+    dy = min(a_y2, b_y2) - max(a_y1, b_y1)
+    if (dx >= 0) and (dy >= 0):
+        return dx * dy
+    return 0
 
 
 def write_json(filename, data, pretty=False):
